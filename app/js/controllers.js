0db6d7142335aa6b955d72983b76a77655c25739
Jeison caro estrada <jcaroe@ucn.edu.co>
2015-10-13 22:57:14 -0500

    first commit

---
+++ app/js/controllers.js
@@ -0,0 +1,12 @@
+angular.module("MyApp")
+.controller('HomeCtrl',function($scope,$rootScope){
+
+	$scope.controllerMessage = 'This is a message from the controller';
+	
+})
+.controller('LoginCtrl',function($scope,$rootScope){
+
+	$scope.controllerMessage = 'This is a message from the controller';
+	
+})
+
